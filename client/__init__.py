e45b324fec7e46d6755464335c5078a0d0741e4f
Tiago Alves Macambira <tmacam@burocrata.org>
2008-11-13 19:57:32 +0000

    Improving __init__.py from client and server modules

--- client/__init__.py
+++ client/__init__.py
@@ -1,2 +1,10 @@
+from client import BaseClient, getUUID, log_backtrace, log_urllib2_exception
+
+# import important modules and bring client.py main classes into this
+# namespace -- saves time, and avoids DistributedCrawler.client.client imports
+
 __all__ = ["client", "article_retriever", "uuid", "daemonize", 
-        "BeautifulSoup", "upload_aux"]
+        "BeautifulSoup", "upload_aux",
+        # client.py internal symbols
+        "BaseClient", "getUUID", "log_backtrace", "log_urllib2_exception"
+        ]
--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,8 @@
-__all__ = ["server", "scheduler"]
+from server import BaseControler, BaseDistributedCrawlingServer, ClientRegistry, GdbmBaseControler, InvalidClientId, ManageScheduler, Ping
+
+# import important modules and bring server.py main classes into this
+# namespace -- saves time, and avoids DistributedCrawler.server.server imports
+
+__all__ = ["server", "scheduler", "BaseControler",
+        "BaseDistributedCrawlingServer", "ClientRegistry", "GdbmBaseControler",
+        "InvalidClientId", "ManageScheduler", "Ping"]
