b64666e7cef1ab54f2e67e5e833ef9e4588d7cf5
Tiago Alves Macambira <tmacam@burocrata.org>
2009-04-08 20:18:15 +0000

    Small fixes regarding Berkeley DB integration (2)

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from server import BaseControler, BaseDistributedCrawlingServer, ClientRegistry, GdbmBaseControler, InvalidClientId, ManageScheduler, Ping
+from server import BaseControler, BaseDistributedCrawlingServer, ClientRegistry, GdbmBaseControler, InvalidClientId, ManageScheduler, Ping, BsddbBaseControler
 
 # import important modules and bring server.py main classes into this
 # namespace -- saves time, and avoids DistributedCrawler.server.server imports
